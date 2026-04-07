a34c80fe6915b0ec4f1f8bc504d60bb5bed3fa5e
Mohosin999 <mohosin.hasan.akash@gmail.com>
2026-04-07 23:36:50 +0600

    some change on middleware cors origin

--- backend/src/middleware/index.ts
+++ backend/src/middleware/index.ts
@@ -1,3 +1,25 @@
+// import "dotenv/config";
+// import express, { Application } from "express";
+// import passport from "passport";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+
+// import "../config/passport";
+
+// export const applyMiddleware = (app: Application): void => {
+//   app.use(express.json({ limit: "10mb" }));
+//   app.use(cookieParser());
+//   app.use(express.urlencoded({ extended: true }));
+//   app.use(
+//     cors({
+//       origin: process.env.FRONTEND_ORIGIN,
+//       credentials: true,
+//     })
+//   );
+
+//   app.use(passport.initialize());
+// };
+
 import "dotenv/config";
 import express, { Application } from "express";
 import passport from "passport";
@@ -12,9 +34,13 @@ export const applyMiddleware = (app: Application): void => {
   app.use(express.urlencoded({ extended: true }));
   app.use(
     cors({
-      origin: process.env.FRONTEND_ORIGIN,
+      origin: [
+        process.env.FRONTEND_ORIGIN,
+        "http://localhost:5173",
+        "http://localhost:4173",
+      ],
       credentials: true,
-    })
+    }),
   );
 
   app.use(passport.initialize());
